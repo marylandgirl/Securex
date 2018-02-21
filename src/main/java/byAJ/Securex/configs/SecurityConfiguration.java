e20aa34cf0ff765813a9e3c8a42874249b5c1c07
Kim Levin <klevin5@student.umuc.edu>
2018-02-21 10:54:55 -0500

    Made it possible to view book listings without logging in

--- src/main/java/byAJ/Securex/configs/SecurityConfiguration.java
+++ src/main/java/byAJ/Securex/configs/SecurityConfiguration.java
@@ -18,7 +18,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().authenticated();
+                .authorizeRequests()
+                .antMatchers("/","/books/list").permitAll()
+                .anyRequest().authenticated();
         http
                 .formLogin().failureUrl("/login?error")
                 .defaultSuccessUrl("/")
